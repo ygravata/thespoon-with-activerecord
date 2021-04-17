95d375e1e59a09ece971244364bcb74e7b6b6c98
Yuri Gravata <ygravata@gmail.com>
2021-04-16 21:46:52 -0300

    adding show action and seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+100.times do
+  Restaurant.create!(
+    name: Faker::Restaurant.name,
+    address:Faker::Address.street_address,
+    rating: rand(1..5)
+  )
+end
