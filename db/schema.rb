d698ca835c44052750d76b15b44267869599afa5
Yuri Gravata <ygravata@gmail.com>
2021-04-16 21:21:48 -0300

    address to restaurant migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_17_002000) do
+ActiveRecord::Schema.define(version: 2021_04_17_002049) do
 
   create_table "restaurants", force: :cascade do |t|
     t.string "name"
     t.integer "rating"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "address"
   end
 
 end
